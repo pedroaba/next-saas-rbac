5d71154ac08b3cc817124fa231fe1f43619536eb
Pedro Augusto B. Aparecido <pedr.augustobarbosa.aparecido@gmail.com>
2024-07-21 11:29:13 -0300

    feat: auth library

---
+++ packages/auth/src/models/organization.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod'
+
+export const organizationSchema = z.object({
+  id: z.string(),
+  ownerId: z.string(),
+
+  __typename: z.literal('Organization').default('Organization'),
+})
+
+export type Organization = z.infer<typeof organizationSchema>
---
+++ packages/auth/src/permissions.ts
@@ -0,0 +1,35 @@
+import type { AbilityBuilder } from '@casl/ability'
+
+import type { AppAbility } from '.'
+import type { User } from './models/user'
+import type { Role } from './roles'
+
+type PermissionByRole = (
+  user: User,
+  builder: AbilityBuilder<AppAbility>,
+) => void
+
+export const permissions: Record<Role, PermissionByRole> = {
+  ADMIN: (user, { can, cannot }) => {
+    can('manage', 'all')
+
+    cannot(['transfer_ownership', 'update'], 'Organization')
+    can(['transfer_ownership', 'update'], 'Organization', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
+  },
+  MEMBER: (user, { can }) => {
+    can(['get'], 'User')
+    can(['get', 'create'], 'Project')
+    can(['update', 'delete'], 'Project', {
+      ownerId: {
+        $eq: user.id,
+      },
+    })
+  },
+  BILLING: (_, { can }) => {
+    can('manage', 'Billing')
+  },
+}
